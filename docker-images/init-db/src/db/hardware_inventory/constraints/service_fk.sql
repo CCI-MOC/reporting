e3eebc4167ce2086ae52815ba565ed4c8a22ef1a
Robert Baron <robbaron@bu.edu>
2021-01-31 23:07:39 -0500

    Deleting one of the docker images as it is not required for the build prcess.

--- docker-images/init-db/src/db/hardware_inventory/constraints/service_fk.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE hardware_inventory ADD CONSTRAINT service_hardware_inventory_fk
-FOREIGN KEY (service_id)
-REFERENCES service (service_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
--- docker-images/init-db/src/db/institution/table.sql
+++
@@ -1,8 +0,0 @@
-
-DROP TABLE if exists institution cascade;
-
-CREATE TABLE institution (
-                institution_id BIGINT NOT NULL,
-                institution_name VARCHAR(200),
-                CONSTRAINT institution_pk PRIMARY KEY (institution_id)
-);
