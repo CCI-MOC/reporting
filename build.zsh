ce88d702a6a99e1f7ba2e78b772964dadd0168b2
Robert Baron <robbaron@bu.edu>
2021-03-05 15:23:13 -0500

    the script that drives the deployment of this project onto OpenShift.

--- build.zsh
+++ build.zsh
@@ -1,44 +1,46 @@
 #!=/usr/local/bin/zsh
-project_name="reporting-1"
-postgres_dbname="reporting-1"
+project_name="reporting-5"
+postgres_dbname="postgres"
 postgres_username="admin"
 postgres_password="pass"
 database_size="2Gi"
 # image: docker-registry.default.svc:5000/[project_name]/init-db:latest
 # image: docker.io/robertbartlettbaron/init-db:latest
 
-
+#create stuff into the project
 oc new-project $project_name
-oc create configmap postgres-setup --"from-file=PostgresSetup/init_rpt.sql"
-oc process -f yaml/postgres-secret.yaml -p project_name=$project_name -p pg_user=$postgres_username -p pg_pass=$postgres_password -p pg_db=$postgres_dbname | oc create -f -
-oc process -f yaml/postgres-pvc.yaml -p project_name=$project_name -p db_size=$database_size | oc create -f -
-oc process -f yaml/postgres-deployment.yaml  -p project_name=$project_name | oc apply -f -
-oc process -f yaml/postgres-service.yaml -p project_name=$project_name | oc apply -f -
-
-# oc process -f yaml/postgres-secret-patch.yaml -p project_name=$project_name | oc patch -f -
-# oc process -f yaml/rpt-init-db-job.yaml -p project_name=$project_name | oc apply -f -
-# oc process -f yaml/init-db-only.yaml -p project_name=$project_name | oc apply -f -
+oc create configmap reporting-db-initdb --"from-file=PostgresSetup/init_rpt.sql"
+oc create configmap billing-config --"from-file=docker-images/get-info/billconf.json"
+oc process -f yaml/pg-rpt-secret.yaml -p project_name=$project_name -p pg_user=$postgres_username -p pg_pass=$postgres_password -p pg_db=$postgres_dbname | oc create -f -
+oc process -f yaml/pg-rpt-conn.yaml -p project_name=$project_name -p pg_user=$postgres_username -p pg_pass=$postgres_password -p pg_db=$postgres_dbname | oc create -f -
 
 
+#copy the kustomize stuff to the working directory
+mkdir work
+sed "s/\${project_name}/$project_name/g" yaml/kustomize-template.yaml > work/kustomization.yaml
+cp yaml/postgres-pvc.yaml work/postgres-pvc.yaml
+cp yaml/postgres-deployment.yaml work/postgres-deployment.yaml
+cp yaml/postgres-service.yaml work/postgres-service.yaml
+cp yaml/secret-generator.yaml work/secret-generator.yaml
+cp yaml/test-conn.yaml work/test-conn.yaml
+cp yaml/get-info.yaml work/get-info.yaml
 
-# psql -d "host=reporting-1-rpt-db-svc port=5432 dbname=postgres user=admin"
-# psql -d "host=172.30.167.60 port=5432 dbname=postgres user=admin"
-# psql -d "host=localhost port=5432 dbname=postgres user=admin"
-#
-# (app-root) sh-4.2$ env | grep POSTGRES
-# POSTGRESQL_PORT_5432_TCP_ADDR=172.30.186.103
-# POSTGRESQL_PORT=tcp://172.30.186.103:5432
-# POSTGRESQL_SERVICE_PORT_POSTGRESQL=5432
-# POSTGRESQL_PORT_5432_TCP=tcp://172.30.186.103:5432
-# POSTGRESQL_SERVICE_HOST=172.30.186.103
-# POSTGRESQL_PORT_5432_TCP_PORT=5432
-# POSTGRESQL_SERVICE_PORT=5432
-# POSTGRESQL_PORT_5432_TCP_PROTO=tcp
+cd work
+oc apply -k . 
+cd ..
+
+# "psql", "-d" ,"host=$DATABASE_HOST port=$POSTGRES_PORT dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD", "-c", "\q"
+# "psql", "-d" ,"host=reporting-db-svc port=5432 dbname=postgres user=admin password=pass", "-c", '\q'
+# "sleep", 60000
+# psql -d "host=reporting-db-svc port=5432 dbname=postgres user=admin password=pass" -c "\q"
+
+POSTGRES_PASSWORD=pass
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/postgresql/13/bin
+POSTGRES_HOST=reporting-db-svc
+POSTGRES_USER=admin
+POSTGRES_PORT=5432
+POSTGRES_DATABASE=postgres
+
+# psql -d "host=$POSTGRES_HOST port=$POSTGRES_PORT dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD" -c "\q"
 
 
-# {"apiVersion":"v1","kind":"Service","metadata":{"annotations":{},"name":"reporting-1-rpt-db-svc","namespace":"reporting-1"},"spec":{"ports":[{"name":"postgresql","port":5432,"protocol":"TCP","targetPort":5432}],"selector":{"app":"reporting-1","deploymentconfig":"reporting-1-dc","name":"postgres"},"type":"ClusterIP"},"status":{"loadBalancer":{}}}
-# 
-# psql -d "host=reporting-1-rpt-db-svc port=5432 dbname=postgres user=admin"
-# psql: error: could not connect to server: Connection refused
-#        Is the server running on host "reporting-1-rpt-db-svc" (172.30.167.60) and accepting
-#        TCP/IP connections on port 5432?
